bc353043fc9fb565bf8ed2ee86ff76c81e847a7a
Joey <joey.imbasciano@gmail.com>
2012-08-19 23:11:40 +0000

    Update run_siege.sh with better default values for load testing.

--- run_siege.sh
+++ run_siege.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # Run Siege for 10 minutes with a single request pointed at the ELB configured
 # for AutoScaling at AWS.
+
+# Configure how many 64byte blocks to write using DD. I've found the sweet spot
+# to be about 25000 in this proof of concept. t1.micros are tricky though
+# because not all are created equally. If this number is too high then the
+# t1.micro will spin out of control. If this number is too low, then
+# autoscaling won't kick in.
+COUNT=25000
+
 CMD=`which siege`
 if [ "$?" == "1" ]; then
   echo "siege is not installed or not in your current path."
@@ -9,4 +17,4 @@ if [ "$?" == "1" ]; then
 fi
 
 echo "Running AutoScaling Proof of Concept"
-$CMD -c1 -t10M nodeload.catsatemybacon.net/?count=50000
+$CMD -c1 -t10M nodeload.catsatemybacon.net/?count=$COUNT
